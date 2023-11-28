feb822f084147723fd77419ba378da3afd4548da
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-11-28 20:31:04 +0200

    knowing sign of num

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number < 1:
+if number < 0:
     print(f"{number:d} is negative")
-elif number > 1:
+elif number > 0:
     print(f"{number:d} is positive")
 else:
     print(f"{number:d} is zero")
