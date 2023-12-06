7778c6112dc0507727552e1ee10f6e36ad26453b
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-12-06 19:02:36 +0200

    sort dictionary keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,7 +6,6 @@ def print_sorted_dictionary(a_dictionary):
     this function sort keys of dictionary
     according to alphabets order
     """
-    
     keys = list(a_dictionary.keys())
     keys.sort()
     for key in keys:
