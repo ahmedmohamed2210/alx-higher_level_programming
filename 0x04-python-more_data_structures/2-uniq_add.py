287a5af74455161ab1241ad1f14e608765371317
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-12-06 17:51:46 +0200

    add uniq num

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -7,6 +7,6 @@ def uniq_add(my_list=[]):
     for i in my_list:
         if i not in new_list:
             sum += i
-            new_list.apppend(i)
+            new_list.append(i)
     return sum
 
