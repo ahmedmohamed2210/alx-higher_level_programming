dc57dea2fddadcda9692e8dd380ece82a30629f1
ahmedmohamed2210 <halawaahmed507@gmail.com>
2024-04-09 04:40:54 +0200

    m

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-function factorial (num) {
-  if (num === 1) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
-  }
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-if (isNaN(process.argv[2])) {
-  console.log(1);
-} else {
-  console.log(factorial(parseInt(process.argv[2])));
-}
+console.log(factorial(Number(process.argv[2])));
